f660c2bc76336da9ca95fba9a78ea3b920b2524a
guilllermoamador <guillermoamador.685@gmail.com>
2026-07-12 22:45:58 -0600

    Cree la entidad usuario

---
+++ src/main/java/mx/uam/ayd/proyecto/negocio/modelo/Vendedor.java
@@ -0,0 +1,11 @@
+package mx.uam.ayd.proyecto.negocio.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Vendedor {
+
+}
