1f81f6f78868fe3e56743654aac9843915c6eebe
yamelinlarios-art <yamelinlarios@gmail.com>
2026-07-15 11:50:39 -0600

    producto modificado

--- src/main/java/mx/uam/ayd/proyecto/negocio/modelo/Producto.java
+++ src/main/java/mx/uam/ayd/proyecto/negocio/modelo/Producto.java
@@ -4,11 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
+/**
+ * Entidad de negocio Producto
+ *
+ * @author Yamelin, Guillermo, Dydier, Yael, Sheyla
+ *
+ */
 @Entity
 public class Producto {
 
