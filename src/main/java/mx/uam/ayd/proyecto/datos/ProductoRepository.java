068cdf633acc5fd382064696013afa2819af0fde
Dyd1er <kevindydierlopezflores@gmail.com>
2026-07-17 20:57:10 -0600

    Cree el repositorio Producto

---
+++ src/main/java/mx/uam/ayd/proyecto/datos/ProductoRepository.java
@@ -0,0 +1,19 @@
+
+package mx.uam.ayd.proyecto.datos;
+
+import org.springframework.data.repository.CrudRepository;
+import mx.uam.ayd.proyecto.negocio.modelo.Producto;
+
+/**
+ * Repositorio para la entidad Producto.
+ * @author JAVITOS
+ */
+public interface ProductoRepository extends CrudRepository<Producto, Long> {
+
+    /**
+     * Busca un producto específico por su nombre.
+     * @param nombre El nombre del producto (ej. "Martillo")
+     * @return
+     */
+    public Producto findByNombre(String nombre);
+}
