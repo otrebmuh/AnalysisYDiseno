4ceea227758d4ae19308947d42779668fb5e0ef1
Dyd1er <kevindydierlopezflores@gmail.com>
2026-07-17 20:18:58 -0600

    Actualice Entidad Venta

--- src/main/java/mx/uam/ayd/proyecto/negocio/modelo/Venta.java
+++ src/main/java/mx/uam/ayd/proyecto/negocio/modelo/Venta.java
@@ -1,5 +1,4 @@
 package mx.uam.ayd.proyecto.negocio.modelo;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -11,57 +10,92 @@ import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-
 /**
- * Entidad venta
- * @author Kevin Dydier López Flores
- */
+
+* @author Kevin Dydier López Flores
+*/
 @Entity
 public class Venta {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idVenta;
 
-    private LocalDateTime fecha;
+    private Double total;
 
-    private double total;
+    private LocalDateTime date;
 
-    
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
-    @JoinColumn(name = "idVenta") // Crea la llave foránea en la tabla DescripcionVenta
-    private List<DescripcionVenta> detalles = new ArrayList<>();
+    private int idVendedor;
 
+@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+@JoinColumn(name = "idVenta")
+    private List<DescripcionVenta> productos = new ArrayList<>();
     public Venta() {
     }
 
-    public void agregarDetalle(DescripcionVenta detalle) {
-        this.detalles.add(detalle);
-    }
+        public void agregarDetalle(DescripcionVenta detalle) {
+        this.productos.add(detalle);
+        }
 
-    // Getters y Setters
-    public long getIdVenta() {
-        return idVenta; }
-    public void setIdVenta(long idVenta) {
-        this.idVenta = idVenta; }
+        public long getIdVenta() {
+        return idVenta;
+        }
 
-    public LocalDateTime getFecha() {
-        return fecha; }
-    public void setFecha(LocalDateTime fecha) {
-        this.fecha = fecha; }
+        public void setIdVenta(long idVenta) {
+        this.idVenta = idVenta;
+        }
 
-    public double getTotal() {
-        return total; }
-    public void setTotal(double total) {
-        this.total = total; }
+        public Double getTotal() {
+        return total;
+        }
 
-    public List<DescripcionVenta> getDetalles() {
-        return detalles; }
-    public void setDetalles(List<DescripcionVenta> detalles) {
-        this.detalles = detalles; }
+        public void setTotal(Double total) {
+        this.total = total;
+        }
+
+        public LocalDateTime getDate() {
+        return date;
+        }
+
+        public void setDate(LocalDateTime date) {
+        this.date = date;
+        }
+
+        public int getIdVendedor() {
+        return idVendedor;
+        }
+
+        public void setIdVendedor(int idVendedor) {
+        this.idVendedor = idVendedor;
+        }
+
+        public List<DescripcionVenta> getProductos() {
+        return productos;
+        }
+
+        public void setProductos(List<DescripcionVenta> productos) {
+        this.productos = productos;
+        }
 
     @Override
-    public String toString() {
-        return "Venta [id=" + idVenta + ", total=" + total + ", fecha=" + fecha + "]";
+    public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null || getClass() != obj.getClass()) return false;
+    Venta other = (Venta) obj;
+    return idVenta == other.idVenta;
     }
-}
+
+    @Override
+    public int hashCode() {
+    return Long.hashCode(idVenta);
+    }
+    
+    @Override
+    public String toString() {
+    return "Venta{" +
+    "id=" + idVenta +
+    ", total=" + total +
+    ", date=" + date +
+    ", idVendedor=" + idVendedor +
+    '}';
+    }
+    }
