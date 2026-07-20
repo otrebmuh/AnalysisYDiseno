fbdc7adc4f4d52d97bdaa9b4437db026692fe8a8
Dyd1er <kevindydierlopezflores@gmail.com>
2026-07-19 22:33:26 -0600

    Cree inventario Repositorio

---
+++ src/main/java/mx/uam/ayd/proyecto/datos/InventarioRepository.java
@@ -0,0 +1,22 @@
+package mx.uam.ayd.proyecto.datos;
+
+import org.springframework.data.repository.CrudRepository;
+import mx.uam.ayd.proyecto.negocio.modelo.Inventario;
+
+/**
+ * Repositorio para el control de Inventario
+ *
+ * @author KEVIN DYDIER
+ *
+ */
+public interface InventarioRepository extends CrudRepository <Inventario, Long> {
+	
+	/**
+	 * Recupera el registro de inventario de un producto específico
+	 * 
+	 * @param idProducto
+	 * @return el registro de inventario
+	 */
+	public Inventario findByIdProducto(long idProducto);
+
+}
