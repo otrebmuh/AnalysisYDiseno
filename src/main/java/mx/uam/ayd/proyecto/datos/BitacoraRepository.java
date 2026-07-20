4df40255ae9128ec4e70d9118874c8a98712c869
Dyd1er <kevindydierlopezflores@gmail.com>
2026-07-19 22:35:25 -0600

    Cree bitacora Repositorio

---
+++ src/main/java/mx/uam/ayd/proyecto/datos/BitacoraRepository.java
@@ -0,0 +1,20 @@
+
+package mx.uam.ayd.proyecto.datos;
+
+import org.springframework.data.repository.CrudRepository;
+import mx.uam.ayd.proyecto.negocio.modelo.Bitacora;
+
+/**
+ * Repositorio para el registro de acciones en Bitácora
+ * @author KEVIN DYDIER *
+ */
+public interface BitacoraRepository extends CrudRepository <Bitacora, Long> {
+	
+	/**
+	 * Encuentra registros de bitácora asociados a un producto específico
+	 * @param idProducto
+	 * @return lista de eventos relacionados al producto
+	 */
+	public java.util.List<Bitacora> findByIdProducto(long idProducto);
+
+}
