1aa5da29da367687f1c8dbb7967e5721759429a6
yamelinlarios-art <yamelinlarios@gmail.com>
2026-07-22 00:59:43 -0600

    cambio en repositorio bitacora

--- src/main/java/mx/uam/ayd/proyecto/datos/BitacoraRepository.java
+++ src/main/java/mx/uam/ayd/proyecto/datos/BitacoraRepository.java
@@ -1,21 +1,23 @@
 package mx.uam.ayd.proyecto.datos;
 
 import org.springframework.data.repository.CrudRepository;
-import mx.uam.ayd.proyecto.negocio.modelo.Bitacora;
+
+import mx.uam.ayd.proyecto.negocio.modelo.Producto;
 
 /**
- * Repositorio de datos para la entidad Bitacora a
- *
+ * Repositorio para Producto
+ * 
  * @author Yamelin Larios Nepomuseno
  *
  */
-public interface BitacoraRepository extends CrudRepository<Bitacora, Long> {
-    // Al heredar de CrudRepository<Bitacora, Long>:
-    // 1. Bitacora es la entidad a gestionar.
-    // 2. Long es el tipo de dato de su Llave Primaria (@Id -> idBitacora).
-    //
-    // Métodos que Spring te regala automáticamente:
-    // - .save(bitacora)     -> Guarda o actualiza un registro.
-    // - .findById(id)       -> Busca una bitácora por su idBitacora.
-    // - .findAll()          -> Trae la lista completa de la bitácora.
+public interface ProductoRepository extends CrudRepository <Producto, Long> {
+    
+    /**
+     * Encuentra un producto a partir de su idProducto
+     * 
+     * @param idProducto
+     * @return
+     */
+    public Producto findByIdProducto(long idProducto);
+
 }
