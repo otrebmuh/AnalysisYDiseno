31f62a1f712152b52281b68f42589bd6a661dc2a
yamelinlarios-art <yamelinlarios@gmail.com>
2026-07-22 00:52:09 -0600

    modificacion producto

--- src/main/java/mx/uam/ayd/proyecto/negocio/modelo/Producto.java
+++ src/main/java/mx/uam/ayd/proyecto/negocio/modelo/Producto.java
@@ -6,108 +6,103 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 /**
+ * Entidad de negocio Producto
+ *
  * @author Yamelin, Guillermo, Dydier, Yael, Sheyla
+ *
  */
 @Entity
 public class Producto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long idProducto;
-
-    private String clave;
+    private long idProducto; 
 
     private String nombre;
-
     private String tipoProducto;
-
-    private Double precio;
-
+    private Double precio; // Usado para HU09 (Precio de Venta a actualizar)
     private String marca;
-
-    private String categoria;
-
-    private Double precioCompra;
+    private String categoria; // Se cambió de Object a String para evitar problemas de persistencia en JPA
     
-    private int existenciaActual;
-    
+    // Atributos colaborativos (HU de tus compañeros)
+    private Double precioCompra; 
+    private int existenciaActual; 
 
+    // Constructor por defecto requerido por JPA
     public Producto() {
     }
 
-
+    //          MÉTODOS DE ACCESO: GETTERS
 
     public long getIdProducto() {
         return idProducto;
-    }
-
-    public void setIdProducto(long idProducto) {
-        this.idProducto = idProducto;
-    }
-
-    public String getClave() {
-        return clave;
-    }
-
-    public void setClave(String clave) {
-        this.clave = clave;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
     public String getTipoProducto() {
         return tipoProducto;
-    }
-
-    public void setTipoProducto(String tipoProducto) {
-        this.tipoProducto = tipoProducto;
     }
 
     public Double getPrecio() {
         return precio;
     }
 
-    public void setPrecio(Double precio) {
-        this.precio = precio;
-    }
-
     public String getMarca() {
         return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
     }
 
     public String getCategoria() {
         return categoria;
     }
 
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-
     public Double getPrecioCompra() {
         return precioCompra;
-    }
-
-    public void setPrecioCompra(Double precioCompra) {
-        this.precioCompra = precioCompra;
     }
 
     public int getExistenciaActual() {
         return existenciaActual;
     }
 
+
+    //          MÉTODOS DE ACCESO: SETTERS
+
+    public void setIdProducto(long idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTipoProducto(String tipoProducto) {
+        this.tipoProducto = tipoProducto;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setPrecioCompra(Double precioCompra) {
+        this.precioCompra = precioCompra;
+    }
+
     public void setExistenciaActual(int existenciaActual) {
         this.existenciaActual = existenciaActual;
     }
+
+
+    //          MÉTODOS SOBREESCRITOS (@Override)
 
     @Override
     public boolean equals(Object obj) {
@@ -126,8 +121,9 @@ public class Producto {
     public String toString() {
         return "Producto{" +
                 "id=" + idProducto +
-                ", clave='" + clave + '\'' +
                 ", nombre='" + nombre + '\'' +
+                ", precioVenta=" + precio +
+                ", precioCompra=" + precioCompra +
                 ", stock=" + existenciaActual +
                 '}';
     }
