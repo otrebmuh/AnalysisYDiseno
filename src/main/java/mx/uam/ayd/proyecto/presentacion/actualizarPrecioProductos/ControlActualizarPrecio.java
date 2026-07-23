65d3d69cde618ca81f036c0ab7d5958a58340c01
yamelinlarios-art <yamelinlarios@gmail.com>
2026-07-23 13:12:07 -0600

    ventana actualizar precio

--- src/main/java/mx/uam/ayd/proyecto/presentacion/actualizarPrecioProductos/ControlActualizarPrecio.java
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/actualizarPrecioProductos/ControlActualizarPrecio.java
@@ -1,4 +1,4 @@
-package mx.uam.ayd.proyecto.presentacion.controlActualizarPrecio;
+package mx.uam.ayd.proyecto.presentacion.actualizarPrecioProductos;
 
 import org.springframework.stereotype.Component;
 
